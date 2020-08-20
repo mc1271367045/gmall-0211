52b9d82f04ea5527d28e0b5bc4c6fa58e6817250
这样Ba <1271367045@qq.com>
2020-08-20 15:41:05 +0800

    day08（搜索页面）

--- gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
+++ gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
@@ -5,16 +5,16 @@ import com.atguigu.gmall.search.pojo.SearchParamVo;
 import com.atguigu.gmall.search.pojo.SearchResponseVo;
 import com.atguigu.gmall.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Auther: Gork_Mc
  * @Date: 2020/08/18/20:04
  * @Description:
  */
-@RestController
+@Controller
 public class SearchController {
 
     @Autowired
